4abcbe2eac626f889e66c67ade434c4a319e8350
lucaaramos <69743118+lucaaramos@users.noreply.github.com>
2022-06-03 14:43:19 -0300

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,25 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function sumaTodos(array) {
+  // La funcion llamada 'sumaTodos' recibe como argumento un array con dos numeros
+  // y debe devolver la suma total entre todos los numeros dentro de ese rango
+  // ej:
+  // sumaTodos([1,3]) => 1 + 2 + 3 = 6
+  // Nota: Los numeros estan ordenados de menor a mayor.
+
+  // Tu código aca:
+var resultado = array.reduce((acc, elemento)=> {
+  return acc + elemento;
+})
+return resultado;
+
+}
+
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = sumaTodos
